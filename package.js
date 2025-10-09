310db8c419d2e5b2ce58a03ca1b14258e3a987b7
Jayson Ragasa <63760343+jaysonragasa@users.noreply.github.com>
2025-10-09 14:26:13 +0800

    Add files via upload

---
+++ package.js
@@ -0,0 +1,10 @@
+{
+  "name": "snapspot",
+  "version": "1.0.0",
+  "description": "A social media platform but in maps",
+  "scripts": {
+    "build": "node build.js"
+  },
+  "author": "",
+  "license": "ISC"
+}
